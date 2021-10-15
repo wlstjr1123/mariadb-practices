ece11dcddbf15ee6508416bedbe6d5241f8ae76c
wlstjr1123 <wlstjr1123@naver.com>
2021-10-15 11:57:05 +0900

    webdb

--- jdbc-practices/sql/webdb.sql
+++ jdbc-practices/sql/webdb.sql
@@ -3,7 +3,22 @@ desc book;
 desc author;
 
 -- insert author
+insert into author values(null, '스테파니메이어');
+select * from author;
 
 -- insert book
+insert into book values(null, '이클립스', 1, '대여가능');
+select * from book;
+
 
 -- select book
+select a.no, a.title, a.status, b.name as authorname
+	from book a, author b
+where a.author_no = b.no
+order by no desc;
+
+-- update book's status(nom status)
+update book
+	set status = '대여중'
+where no = 1;
+select * from book;
